97d211456d2c038a2a15e6dc89252da05ce0d383
thisisarindam <80046541+thisisarindam@users.noreply.github.com>
2023-12-08 16:44:44 +0000

    variables & dataTypes learned

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+// can not modify const
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "bcd@gmail.com"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
