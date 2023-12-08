dd3b27b22d28ca9b8b97bf9b91b4a593d9edd649
thisisarindam <80046541+thisisarindam@users.noreply.github.com>
2023-12-08 17:03:36 +0000

    dataTypes added

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,22 @@
 "use strict"; //treat all JS code as newer version
+
+// code readability should be high
+
+let name = "arindam"
+let age = 29
+let isLogedIn = false
+let state;
+
+// number
+// bigint
+// string
+// boolean => true/false
+// null => standalone value, can be assigned to variables
+// undefined => not assigned any value
+// symbol => used in reactJs / uniqueness
+
+// object*
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); // object
