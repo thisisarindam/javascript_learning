a23ad1674aca2b98ae3b2869fc7d3cc55e47804a
thisisarindam <80046541+thisisarindam@users.noreply.github.com>
2024-01-29 17:08:44 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "23"
+// //console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// // console.log(typeof (valueInNumber));
+// // console.log(valueInNumber);
+
+// "23" => 23
+// "23abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(typeof (booleanIsloggedIn));
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(typeof (booleanIsloggedIn));
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(typeof (booleanIsloggedIn));
+// console.log(booleanIsLoggedIn);
+
+// let someNumber = 33
+// let numberToString = String(someNumber); //converts number to string
+// console.log(typeof (numberToString));
+// let stringToNumber = Number(numberToString); //converts string to number
+// console.log(typeof (stringToNumber)); 
